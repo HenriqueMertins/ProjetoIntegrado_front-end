92aea263c2aa2312ed808608f2b1e68ee4f8addf
Henrique Mertins <henriquemertins@gmail.com>
2023-09-29 22:39:50 -0300

    adicionado o listview "rolagem de tela"

--- lib/pages/pupil/calendar.dart
+++ lib/pages/pupil/calendar.dart
@@ -36,213 +36,217 @@ class _calendarPageState extends State<calendarPage> {
         title: const Text("Training Calendar"),
         backgroundColor: const Color.fromARGB(255, 196, 188, 188),
       ),
-      body: Column(
+      body: ListView(
         children: [
-          Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: TableCalendar(
-              locale: "en_US",
-              rowHeight: 43,
-              headerStyle: const HeaderStyle(
-                formatButtonVisible: false,
-                titleCentered: true,
+          Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: TableCalendar(
+                  locale: "en_US",
+                  rowHeight: 43,
+                  headerStyle: const HeaderStyle(
+                    formatButtonVisible: false,
+                    titleCentered: true,
+                  ),
+                  availableGestures: AvailableGestures.all,
+                  selectedDayPredicate: (day) => isSameDay(day, today),
+                  focusedDay: today,
+                  firstDay: DateTime.utc(2010, 10, 16),
+                  lastDay: DateTime.utc(2030, 3, 14),
+                  onDaySelected: _onDaySelected,
+                ),
               ),
-              availableGestures: AvailableGestures.all,
-              selectedDayPredicate: (day) => isSameDay(day, today),
-              focusedDay: today,
-              firstDay: DateTime.utc(2010, 10, 16),
-              lastDay: DateTime.utc(2030, 3, 14),
-              onDaySelected: _onDaySelected,
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 20),
-            child: Container(
-              color: Colors.white,
-              padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 10),
-              child: Row(
-                children: [
-                  const Expanded(
-                    child: Text(
-                      "Pular Corda",
-                      style: TextStyle(fontSize: 18),
-                    ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 20),
+                child: Container(
+                  color: Colors.white,
+                  padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 10),
+                  child: Row(
+                    children: [
+                      const Expanded(
+                        child: Text(
+                          "Pular Corda",
+                          style: TextStyle(fontSize: 18),
+                        ),
+                      ),
+                      Container(
+                        width: 100,
+                        height: 50,
+                        color: const Color.fromARGB(255, 100, 100, 100),
+                        alignment: Alignment.center,
+                        child: const Text(
+                          "5 min",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    ],
                   ),
-                  Container(
-                    width: 100,
-                    height: 50,
-                    color: const Color.fromARGB(255, 100, 100, 100),
-                    alignment: Alignment.center,
-                    child: const Text(
-                      "5 min",
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ),
-                ],
+                ),
               ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 20),
-            child: Container(
-              color: Colors.white,
-              padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 10),
-              child: Row(
-                children: [
-                  const Expanded(
-                    child: Text(
-                      "Simulador Escada",
-                      style: TextStyle(fontSize: 18),
-                    ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 20),
+                child: Container(
+                  color: Colors.white,
+                  padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 10),
+                  child: Row(
+                    children: [
+                      const Expanded(
+                        child: Text(
+                          "Simulador Escada",
+                          style: TextStyle(fontSize: 18),
+                        ),
+                      ),
+                      Container(
+                        width: 100,
+                        height: 50,
+                        color: const Color.fromARGB(255, 100, 100, 100),
+                        alignment: Alignment.center,
+                        child: const Text(
+                          "5 min",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    ],
                   ),
-                  Container(
-                    width: 100,
-                    height: 50,
-                    color: const Color.fromARGB(255, 100, 100, 100),
-                    alignment: Alignment.center,
-                    child: const Text(
-                      "5 min",
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ),
-                ],
+                ),
               ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 20),
-            child: Container(
-              color: Colors.white,
-              padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 10),
-              child: Row(
-                children: [
-                  const Expanded(
-                    child: Text(
-                      "Supino reto com halteres",
-                      style: TextStyle(fontSize: 18),
-                    ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 20),
+                child: Container(
+                  color: Colors.white,
+                  padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 10),
+                  child: Row(
+                    children: [
+                      const Expanded(
+                        child: Text(
+                          "Supino reto com halteres",
+                          style: TextStyle(fontSize: 18),
+                        ),
+                      ),
+                      Container(
+                        width: 100,
+                        height: 50,
+                        color: const Color.fromARGB(255, 100, 100, 100),
+                        alignment: Alignment.center,
+                        child: const Text(
+                          "12x de 35kg  4 series",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    ],
                   ),
-                  Container(
-                    width: 100,
-                    height: 50,
-                    color: const Color.fromARGB(255, 100, 100, 100),
-                    alignment: Alignment.center,
-                    child: const Text(
-                      "12x de 35kg  4 series",
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ),
-                ],
+                ),
               ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 20),
-            child: Container(
-              color: Colors.white,
-              padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 10),
-              child: Row(
-                children: [
-                  const Expanded(
-                    child: Text(
-                      "Supino inclinado com halteres",
-                      style: TextStyle(fontSize: 18),
-                    ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 20),
+                child: Container(
+                  color: Colors.white,
+                  padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 10),
+                  child: Row(
+                    children: [
+                      const Expanded(
+                        child: Text(
+                          "Supino inclinado com halteres",
+                          style: TextStyle(fontSize: 18),
+                        ),
+                      ),
+                      Container(
+                        width: 100,
+                        height: 50,
+                        color: const Color.fromARGB(255, 100, 100, 100),
+                        alignment: Alignment.center,
+                        child: const Text(
+                          "12x de 27kg  4 series",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    ],
                   ),
-                  Container(
-                    width: 100,
-                    height: 50,
-                    color: const Color.fromARGB(255, 100, 100, 100),
-                    alignment: Alignment.center,
-                    child: const Text(
-                      "12x de 27kg  4 series",
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ),
-                ],
+                ),
               ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 20),
-            child: Container(
-              color: Colors.white,
-              padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 10),
-              child: Row(
-                children: [
-                  const Expanded(
-                    child: Text(
-                      "Flexão",
-                      style: TextStyle(fontSize: 18),
-                    ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 20),
+                child: Container(
+                  color: Colors.white,
+                  padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 10),
+                  child: Row(
+                    children: [
+                      const Expanded(
+                        child: Text(
+                          "Flexão",
+                          style: TextStyle(fontSize: 18),
+                        ),
+                      ),
+                      Container(
+                        width: 100,
+                        height: 50,
+                        color: const Color.fromARGB(255, 100, 100, 100),
+                        alignment: Alignment.center,
+                        child: const Text(
+                          "12 rep",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    ],
                   ),
-                  Container(
-                    width: 100,
-                    height: 50,
-                    color: const Color.fromARGB(255, 100, 100, 100),
-                    alignment: Alignment.center,
-                    child: const Text(
-                      "12 rep",
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ),
-                ],
+                ),
               ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 20),
-            child: Container(
-              color: Colors.white,
-              padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 10),
-              child: Row(
-                children: [
-                  const Expanded(
-                    child: Text(
-                      "Triceps Polia Alta",
-                      style: TextStyle(fontSize: 18),
-                    ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 20),
+                child: Container(
+                  color: Colors.white,
+                  padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 10),
+                  child: Row(
+                    children: [
+                      const Expanded(
+                        child: Text(
+                          "Triceps Polia Alta",
+                          style: TextStyle(fontSize: 18),
+                        ),
+                      ),
+                      Container(
+                        width: 100,
+                        height: 50,
+                        color: const Color.fromARGB(255, 100, 100, 100),
+                        alignment: Alignment.center,
+                        child: const Text(
+                          "10x de 30kg  4 series",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    ],
                   ),
-                  Container(
-                    width: 100,
-                    height: 50,
-                    color: const Color.fromARGB(255, 100, 100, 100),
-                    alignment: Alignment.center,
-                    child: const Text(
-                      "10x de 30kg  4 series",
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ),
-                ],
+                ),
               ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 20),
-            child: Container(
-              color: Colors.white,
-              padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 10),
-              child: Row(
-                children: [
-                  const Expanded(
-                    child: Text(
-                      "Triceps Francês Unilateral",
-                      style: TextStyle(fontSize: 18),
-                    ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 20),
+                child: Container(
+                  color: Colors.white,
+                  padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 10),
+                  child: Row(
+                    children: [
+                      const Expanded(
+                        child: Text(
+                          "Triceps Francês Unilateral",
+                          style: TextStyle(fontSize: 18),
+                        ),
+                      ),
+                      Container(
+                        width: 100,
+                        height: 50,
+                        color: const Color.fromARGB(255, 100, 100, 100),
+                        alignment: Alignment.center,
+                        child: const Text(
+                          "10x de 22kg  4 series",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    ],
                   ),
-                  Container(
-                    width: 100,
-                    height: 50,
-                    color: const Color.fromARGB(255, 100, 100, 100),
-                    alignment: Alignment.center,
-                    child: const Text(
-                      "10x de 22kg  4 series",
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ),
-                ],
+                ),
               ),
-            ),
+            ],
           ),
         ],
       ),
