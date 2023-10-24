12d45bf368b1a09243f01398d235ecf13fbcd32c
mertins <luciano@mertins.com.br>
2023-10-23 23:35:29 -0300

    Imprimindo na console do flutter.  ListPupil linha 26

--- lib/pages/professor/ListPupil.dart
+++ lib/pages/professor/ListPupil.dart
@@ -44,60 +44,60 @@ class _listPupilState extends State<ListPupil> {
       ),
       backgroundColor: const Color.fromARGB(255, 196, 188, 188),
 
-      // body: Center(
-      //   child: FutureBuilder<List<PupilDTO>>(
-      //     future: _pupils(),
-      //     builder: (context, snapshot) {
-      //       if (snapshot.hasData) {
-      //         return Text(snapshot.data!.first.nome);
-      //       } else if (snapshot.hasError) {
-      //         return Text('${snapshot.error}');
-      //       }
-      //
-      //       // By default, show a loading spinner.
-      //       return const CircularProgressIndicator();
-      //     },
-      //   ),
-      // ),
-
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'Lista de Alunos',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 20),
-            Container(
-              width: double.infinity,
-              color: const Color.fromARGB(255, 255, 255, 255),
-              margin: const EdgeInsets.symmetric(horizontal: 16.0),
-              padding: const EdgeInsets.all(16.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  const Text(
-                    "Nome do Aluno",
-                    style: TextStyle(
-                      color: Color.fromARGB(255, 0, 0, 0),
-                      fontSize: 16.0,
-                    ),
-                  ),
-                  IconButton(
-                    icon: const Icon(Icons.keyboard_arrow_down,
-                        color: Color.fromARGB(255, 0, 0, 0)),
-                    onPressed: () =>
-                        {Navigator.of(context).pushNamed("/InfoPupil")},
-                  ),
-                ],
-              ),
-            ),
-            const SizedBox(height: 20),
-          ],
+        child: FutureBuilder<List<dynamic>>(
+          future: _pupils(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return Text(snapshot.data.toString());
+            } else if (snapshot.hasError) {
+              return Text('${snapshot.error}');
+            }
+
+            // By default, show a loading spinner.
+            return const CircularProgressIndicator();
+          },
         ),
       ),
+
+      // body: Center(
+      //   child: Column(
+      //     mainAxisAlignment: MainAxisAlignment.center,
+      //     crossAxisAlignment: CrossAxisAlignment.center,
+      //     children: <Widget>[
+      //       const Text(
+      //         'Lista de Alunos',
+      //         style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+      //       ),
+      //       const SizedBox(height: 20),
+      //       Container(
+      //         width: double.infinity,
+      //         color: const Color.fromARGB(255, 255, 255, 255),
+      //         margin: const EdgeInsets.symmetric(horizontal: 16.0),
+      //         padding: const EdgeInsets.all(16.0),
+      //         child: Row(
+      //           mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      //           children: [
+      //             const Text(
+      //               "Nome do Aluno",
+      //               style: TextStyle(
+      //                 color: Color.fromARGB(255, 0, 0, 0),
+      //                 fontSize: 16.0,
+      //               ),
+      //             ),
+      //             IconButton(
+      //               icon: const Icon(Icons.keyboard_arrow_down,
+      //                   color: Color.fromARGB(255, 0, 0, 0)),
+      //               onPressed: () =>
+      //                   {Navigator.of(context).pushNamed("/InfoPupil")},
+      //             ),
+      //           ],
+      //         ),
+      //       ),
+      //       const SizedBox(height: 20),
+      //     ],
+      //   ),
+      // ),
     );
   }
 }
