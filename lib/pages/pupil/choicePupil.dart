46fe72f5901eacb0b91cc429065f23f47a794ddf
Henrique Mertins <henriquemertins@gmail.com>
2023-11-12 22:04:31 -0300

    agora sim o list view na pagina certa

--- lib/pages/pupil/choicePupil.dart
+++ lib/pages/pupil/choicePupil.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 // ignore: camel_case_types
 class choicePupil extends StatefulWidget {
-  const choicePupil({super.key});
+  const choicePupil({Key? key});
 
   @override
   State<choicePupil> createState() => _choicePupilPageState();
@@ -27,86 +27,90 @@ class _choicePupilPageState extends State<choicePupil> {
       backgroundColor: const Color.fromARGB(255, 196, 188, 188),
       body: Padding(
         padding: const EdgeInsets.all(10),
-        child: Center(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
+        child: ListView(
+          children: [
+            Center(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
-                  Padding(
-                    padding: EdgeInsets.only(top: 1.0),
-                    child: Icon(
-                      Icons.fitness_center,
-                      color: Color.fromARGB(255, 0, 0, 0),
-                      size: 300.0,
+                  const Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: <Widget>[
+                      Padding(
+                        padding: EdgeInsets.only(top: 1.0),
+                        child: Icon(
+                          Icons.fitness_center,
+                          color: Color.fromARGB(255, 0, 0, 0),
+                          size: 300.0,
+                        ),
+                      ),
+                    ],
+                  ),
+                  const Center(
+                    child: Text(
+                      "Training Calendar", 
+                      style: TextStyle(
+                        fontSize: 24.0, 
+                        fontWeight: FontWeight.bold, 
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 100.0),
+                  ButtonTheme(
+                    height: 60.0,
+                    child: ElevatedButton(
+                      onPressed: () => {
+                        Navigator.of(context).pushNamed("/calendarPupil")
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color.fromARGB(
+                            255, 243, 16, 0), 
+                      ),
+                      child: const Text(
+                        "Minha Agenda",
+                        style: TextStyle(color: Color.fromARGB(255, 255, 255, 255)),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 60.0),
+                  ButtonTheme(
+                    height: 60.0,
+                    child: ElevatedButton(
+                      onPressed: () => {
+                        Navigator.of(context).pushNamed("/MyProfessor")
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color.fromARGB(
+                            255, 243, 16, 0), 
+                      ),
+                      child: const Text(
+                        "Meu Professor",
+                        style: TextStyle(color: Color.fromARGB(255, 255, 255, 255)),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 60.0),
+                  ButtonTheme(
+                    height: 60.0,
+                    child: ElevatedButton(
+                      onPressed: () => {
+                        Navigator.of(context).pushNamed("/myDatePupil")
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color.fromARGB(
+                            255, 243, 16, 0), 
+                      ),
+                      child: const Text(
+                        "Meus Dados",
+                        style: TextStyle(color: Color.fromARGB(255, 255, 255, 255)),
+                      ),
                     ),
                   ),
                 ],
               ),
-              const Center(
-                child: Text(
-                  "Training Calendar", 
-                  style: TextStyle(
-                    fontSize: 24.0, 
-                    fontWeight: FontWeight.bold, 
-                  ),
-                ),
-              ),
-              const SizedBox(height: 100.0),
-              ButtonTheme(
-                height: 60.0,
-                child: ElevatedButton(
-                  onPressed: () => {
-                    Navigator.of(context).pushNamed("/calendarPupil")
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: const Color.fromARGB(
-                        255, 243, 16, 0), 
-                  ),
-                  child: const Text(
-                    "Minha Agenda",
-                    style: TextStyle(color: Color.fromARGB(255, 255, 255, 255)),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 60.0),
-              ButtonTheme(
-                height: 60.0,
-                child: ElevatedButton(
-                  onPressed: () => {
-                    Navigator.of(context).pushNamed("/MyProfessor")
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: const Color.fromARGB(
-                        255, 243, 16, 0), 
-                  ),
-                  child: const Text(
-                    "Meu Professor",
-                    style: TextStyle(color: Color.fromARGB(255, 255, 255, 255)),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 60.0),
-              ButtonTheme(
-                height: 60.0,
-                child: ElevatedButton(
-                  onPressed: () => {
-                    Navigator.of(context).pushNamed("/myDatePupil")
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: const Color.fromARGB(
-                        255, 243, 16, 0), 
-                  ),
-                  child: const Text(
-                    "Meus Dados",
-                    style: TextStyle(color: Color.fromARGB(255, 255, 255, 255)),
-                  ),
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
